c1775742f90663ccc84c0355e2598d7166583110
tortious <unjustlyenriched@protonmail.com>
2024-11-14 10:37:16 -0600

    Create teams-groupANDchannel-creator.ps1

---
+++ PowerShell_Scripts/Sys-admin/teams-groupANDchannel-creator.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module MicrosoftTeams
+Connect-MicrosoftTeams
+$groupDisplayName = "Enter Group Name"
+$mailNickName = "BGISIdeationSessionBPAccount"
+$owners = @("user1@domain.com", "user2@domain.com")
+$channelName = "Enter Channel Name"
+$group = New-Team -DisplayName $groupDisplayName -MailNickName $mailNickName
+foreach ($owner in $owners) {
+    Add-TeamUser -GroupId $group.GroupId -User $owner -Role Owner
+}
+New-TeamChannel -GroupId $group.GroupId -DisplayName $channelName -Description "Enter Channel Description"
